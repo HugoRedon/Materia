b9489c2229db91dbb03e8cbbbc726627a2756a69
HugoRedon <hugo_redon_@hotmail.com>
2014-06-30 00:13:28 -0500

    cambios para guardar listas de datos experimentales de mezclas binarias

--- src/main/java/termo/data/ExperimentalDataBinaryList.java
+++ src/main/java/termo/data/ExperimentalDataBinaryList.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +25,7 @@ public class ExperimentalDataBinaryList implements Serializable{
     private Long id;
     
       
-    @OneToMany(mappedBy = "experimentalDataList")
+    @OneToMany(mappedBy = "experimentalDataList"  ,fetch = FetchType.EAGER)
     private Set<ExperimentalDataBinary> list = new HashSet();
     
     @ManyToOne
