07e30eb4dd53134adfc663b36586e8488604250e
HugoRedon <hugo_redon_@hotmail.com>
2014-08-07 12:36:24 -0500

    indentando

--- src/main/java/termo/matter/Homogeneous.java
+++ src/main/java/termo/matter/Homogeneous.java
@@ -138,27 +138,21 @@ public abstract class  Homogeneous  implements PropertyChangeListener{
     public abstract double oneOver_N_Parcial_a(Substance pureSubstance);
     
     public double calculateMolarVolume(){
-	//return 220.746180;
-	double z = calculateCompresibilityFactor();
-	
-	
-	return cubicEquationOfState.calculateVolume(temperature, pressure, z);
+		double z = calculateCompresibilityFactor();		
+		return cubicEquationOfState.calculateVolume(temperature, pressure, z);
     }
     public double calculateCompresibilityFactor(){
-	double a = calculate_a_cubicParameter();
-	double b = calculate_b_cubicParameter();
-	
-	double A = cubicEquationOfState.get_A(temperature, pressure, a);
-	double B = cubicEquationOfState.get_B(temperature, pressure, b);
-	
-	return  cubicEquationOfState.calculateCompresibilityFactor(A, B, getPhase());
+		double a = calculate_a_cubicParameter();
+		double b = calculate_b_cubicParameter();
+		
+		double A = cubicEquationOfState.get_A(temperature, pressure, a);
+		double B = cubicEquationOfState.get_B(temperature, pressure, b);
+		
+		return  cubicEquationOfState.calculateCompresibilityFactor(A, B, getPhase());
     }
     protected double calculateFugacity( Substance pureSubstance){
         double a = calculate_a_cubicParameter();
         double b = calculate_b_cubicParameter();
-
-	
-	
         double  parcialb = pureSubstance.calculate_b_cubicParameter();//bi(pureSubstance);
         double parciala = oneOver_N_Parcial_a( pureSubstance);
         
@@ -170,14 +164,14 @@ public abstract class  Homogeneous  implements PropertyChangeListener{
         double Temp_parcial_a = partial_aPartial_temperature( );
         
         double L = cubicEquationOfState.calculateL(volume, b);
-	double z = calculateCompresibilityFactor();
+        double z = calculateCompresibilityFactor();
         
         //return idealGasEntropy +  Constants.R * Math.log( (pressure *(volume - b))/(Constants.R * temperature)) + L * (Temp_parcial_a)/(b);
 	return idealGasEntropy +  Constants.R * Math.log( (z *(volume - b))/(volume)) + L * (Temp_parcial_a)/(temperature*b);
     }
     public double calculateEntropy(){
-	double volume = calculateMolarVolume();
-	return calculateEntropy( volume);
+		double volume = calculateMolarVolume();
+		return calculateEntropy( volume);
     }
     private  double calculateEnthalpy( double volume){
         double idealGasEnthalpy = calculateIdealGasEnthalpy();
@@ -189,8 +183,8 @@ public abstract class  Homogeneous  implements PropertyChangeListener{
         return idealGasEnthalpy + ((partial_aPartial_temperature - a)/b) * L  + pressure * volume - Constants.R *temperature;
     }
     public final double calculateEnthalpy(){
-	double volume = calculateMolarVolume();
-	return calculateEnthalpy( volume);
+		double volume = calculateMolarVolume();
+		return calculateEnthalpy( volume);
     }
     public Cubic getCubicEquationOfState() {
         return cubicEquationOfState;
@@ -202,7 +196,7 @@ public abstract class  Homogeneous  implements PropertyChangeListener{
     }
     public abstract double calculatetAcentricFactorBasedVaporPressure();
     public Phase getPhase() {
-	return phase;
+		return phase;
     }
     public void setPhase(Phase phase) {
         Phase oldPhase = this.phase;
@@ -210,13 +204,13 @@ public abstract class  Homogeneous  implements PropertyChangeListener{
         mpcs.firePropertyChange("phase", oldPhase, phase);
     }
     public double calculateGibbs() {
-	double enthalpy = calculateEnthalpy();
-	double entropy = calculateEntropy();
-	
-	return enthalpy - temperature * entropy;
+		double enthalpy = calculateEnthalpy();
+		double entropy = calculateEntropy();
+		
+		return enthalpy - temperature * entropy;
     }
     public double getTemperature() {
-	return temperature;
+    	return temperature;
     }
     public void setTemperature(double temperature) {
         double oldTemperature = this.temperature;
